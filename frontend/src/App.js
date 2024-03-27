ad6b15d8bee26930bc7c8537bf044474771af752
rafapm1999 <rportillom99.code@gmail.com>
2024-03-27 22:47:48 +0100

    Create frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+/* import { Route, Routes } from "react-router-dom"; */
+
+function App() {
+return(
+  <div>
+    <h1>Hola</h1>
+  </div>
+);
+}
+
+export default App;
---
+++ frontend/src/services/LocalStorage.service.js
@@ -0,0 +1,14 @@
+export const LocalStorage = {
+    setItem: (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    getItem: (key) => {
+        return JSON.parse(localStorage.getItem(key));
+    },
+    removeItem: (key) => {
+        localStorage.removeItem(key);
+    },
+    clear: () => {
+        localStorage.clear();
+    }
+}
