bea62a7599e8f20a08f741e4ecccd6125eda8825
rafapm1999 <rportillom99.code@gmail.com>
2024-04-02 23:31:07 +0200

    Create a functional translate morse-string function

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,30 +1,58 @@
 /* import './Home.css'; */
 import { useState } from "react"
+import database from "../../db/morseCodeTranslator.json"
 
 function Home() {
   /* Declaraciones */
-  const [userData, setUserData] = useState([]);
-  const [morseData, setMorseData] = useState();
-  let userStringArray = [];
-  let translateString = "";
+  const [morseData, setMorseData] = useState("");
+  let array = [];
 
   /* Funciones */
-  const translateFunction = () => {
-    setMorseData(userData.split(' '))
-   /*  userStringArray = userData.split('');
-    translateString = String(userStringArray);
-    console.log(translateString); */
+  const sendData = (e) => {
+    if (e) {
+      let morseTranslation = ("")
+      let words = e.split("");
+
+      words.forEach(letters => {
+        array.push(letters.split(""));
+      });
+      array.forEach(word => {
+        word.forEach(letter => {
+          let morseLetter = "";
+
+          if (/[a-z]/.test(letter.toLowerCase())) {
+            morseLetter = database.stringToMorse.letters[letter.toLowerCase()];
+          } else if (/[0-9]/.test(letter)) {
+            morseLetter = database.stringToMorse.numbers[letter];
+          } else if (/[\n&\'@)(:,=!.\-+\"?\/\ ]/.test(letter)) {
+            morseLetter = database.stringToMorse.signals[letter];
+          } else {
+            morseLetter = "ERROR";
+          };
+
+          if (morseLetter) {
+            morseTranslation += morseLetter + " ";
+          } else {
+            if (morseLetter === "\r") {
+              morseTranslation += "";
+            }
+            morseTranslation += " | ";
+          }
+        });
+      });
+      setMorseData(morseTranslation);
+    } else {
+      setMorseData("")
+    }
   }
 
   /* Renderizado */
   return (
     <div>
-      <textarea cols={50} rows={50} onChange={(e) => {
-        setUserData(e.target.value);
+      <textarea placeholder="Introduce el texto a traducir" cols={50} rows={50} onChange={(e) => {
+        sendData(e.target.value)
       }} />
-      <button onClick={translateFunction}>Translate</button>
-      <textarea cols={50} rows={50} value={userData.split(' ')} />
-
+      <textarea cols={50} rows={50} value={morseData === "" ? "Introduce el texto a traducir" : morseData} />
     </div>
   );
 }
