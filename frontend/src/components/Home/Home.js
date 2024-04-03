126dae75a1f83c263893643d017f6c5c64a33119
rafapm1999 <rportillom99.code@gmail.com>
2024-04-04 00:59:15 +0200

    Create readMorse, lightTranslate and lightMode methods

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -5,45 +5,82 @@ import database from "../../db/morseCodeTranslator.json"
 function Home() {
   /* Declaraciones */
   const [morseData, setMorseData] = useState("");
-  let array = [];
+  const [morsePulse, setMorsePulse] = useState("");
+  const [binaryPulse, setBinaryPulse] = useState(false);
+  let morseTranslation = "";
+  let morsePulseArray = [];
 
   /* Funciones */
+
+  /* Funcion de envio de información del usuario para su posterior procesado y traduccion a morse */
   const sendData = (e) => {
     if (e) {
-      let morseTranslation = ("")
-      let words = e.split("");
+      let letterStringArray = e.split("");
+      letterStringArray.forEach(letters => {
+        let morseLetter = "";
 
-      words.forEach(letters => {
-        array.push(letters.split(""));
-      });
-      array.forEach(word => {
-        word.forEach(letter => {
-          let morseLetter = "";
+        if (/[a-z]/.test(letters.toLowerCase())) {
+          morseLetter = database.stringToMorse.letters[letters.toLowerCase()];
+        } else if (/[0-9]/.test(letters)) {
+          morseLetter = database.stringToMorse.numbers[letters];
+        } else if (/[\ &'@)(:,=!.\-+"?/\/]/.test(letters)) {
+          morseLetter = database.stringToMorse.signals[letters];
+        } else {
+          morseLetter = "ERROR";
+        };
 
-          if (/[a-z]/.test(letter.toLowerCase())) {
-            morseLetter = database.stringToMorse.letters[letter.toLowerCase()];
-          } else if (/[0-9]/.test(letter)) {
-            morseLetter = database.stringToMorse.numbers[letter];
-          } else if (/[\n&'@)(:,=!.\-+"?/ ]/.test(letter)) {
-            morseLetter = database.stringToMorse.signals[letter];
-          } else {
-            morseLetter = "ERROR";
-          };
-
-          if (morseLetter) {
-            morseTranslation += morseLetter + " ";
-          } else {
-            if (morseLetter === "\r") {
-              morseTranslation += "";
-            }
-            morseTranslation += " | ";
-          }
-        });
+        if (morseLetter) {
+          morseTranslation += morseLetter + " ";
+        } else {
+          morseTranslation += "| ";
+        }
       });
       setMorseData(morseTranslation);
     } else {
-      setMorseData("")
+      setMorseData("");
     }
+  }
+  /* Funcion con la que analizamos y limpiamos el morse para acabar obteniendo un array de pulsos */
+  const readMorse = () => {
+    if (morseData !== "") {
+      let morseString = morseData;
+      let morsePulsesLetters = "";
+      let morsePulseWordsArray = morseString.split(" | ");
+      console.log(morsePulseWordsArray);
+      morsePulseWordsArray.forEach(morseWord => {
+        console.log(morseWord);
+        if (!morseWord.includes("ERROR")) {
+          morsePulsesLetters += morseWord.trim().replace(/\s/g, "");
+        }
+      });
+      morsePulseArray = morsePulsesLetters.split("")
+      setMorsePulse(morsePulseArray)
+    }
+  }
+
+  const lightTranslation = () => {
+    if (morsePulse !== "") {
+      morsePulse.forEach(pulse => {
+        console.log(pulse);
+        setTimeout(() => {
+          if (pulse === ".") {
+
+            setBinaryPulse(false)
+
+          } else if (pulse === "-") {
+
+            setBinaryPulse(true)
+
+          }
+        }, 1);
+
+      })
+    }
+
+  }
+  async function lightMode() {
+    let read = await readMorse();
+    let translate = await lightTranslation();
   }
 
   /* Renderizado */
@@ -53,6 +90,8 @@ function Home() {
         sendData(e.target.value)
       }} />
       <textarea cols={50} rows={50} value={morseData === "" ? "Introduce el texto a traducir" : morseData} />
+      <button onClick={lightMode}>Light translate</button>
+      <div style={{ height: "40vh", width: "5vw", backgroundColor: binaryPulse === true ? "white" : "black" }}></div>
     </div>
   );
 }
