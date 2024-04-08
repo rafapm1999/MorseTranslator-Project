f9d64a1922e977e908ce4e3e05898868c1e603f0
Rafa Portillo <rportillom99.code@gmail.com>
2024-04-09 00:40:25 +0200

    Create the ux/ui design for the project, create de gainNode controller and fix some problems

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,8 @@
-/* import './Home.css'; */
-import { useState, useEffect} from "react"
-import database from "../../db/morseCodeTranslator.json"
+import './Home.css'; 
+import { useState} from "react";
+import database from "../../db/morseCodeTranslator.json";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faVolumeXmark, faVolumeHigh, faVolumeLow, faVolumeOff, faPause, faPlay, faStop} from "@fortawesome/free-solid-svg-icons"
 
 
 function Home() {
@@ -9,13 +11,31 @@ function Home() {
   const [binaryPulse, setBinaryPulse] = useState("");
   const [speedValue, setSpeedValue] = useState(1000);
   const [pitchValue, setPitchValue] = useState(556);
+  const [volumeValue, setVolumeValue] = useState(0.2);
+  const [muteValue, setMuteValue] = useState(false);
+  const [playValue, setPlayValue] = useState(false);
+  const [stopValue, setStopValue] = useState(false);
   let morseTranslation = "";
   let morsePulseArray = [];
   let audioCtx = new AudioContext();
-
+  const gainNode = audioCtx.createGain();
+  gainNode.gain.value = muteValue === true ? 0 : volumeValue;
+  console.log(muteValue);
   /* Funciones */
 
   /* Funcion de envio de información del usuario para su posterior procesado y traduccion a morse */
+  const inizialiceOsc = (pitchValue, stopValue) => {
+    if (stopValue) {
+      console.log("Stop");
+    } else {
+      const osc = audioCtx.createOscillator();
+      osc.type = "sine";
+      osc.frequency.setValueAtTime(pitchValue, audioCtx.currentTime)
+      osc.connect(gainNode).connect(audioCtx.destination);
+      return osc;
+    }
+    
+  }
  
   const sendData = (e) => {
     if (e !== "") {
@@ -62,85 +82,128 @@ function Home() {
 
   // Función para detener la ejecución durante un tiempo específico
   function speed(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms/2));
   }
 
-
-  const lightTranslation = async () => {
-    if (morsePulseArray.length !== 0) {
-      for (const pulse of morsePulseArray) {
-        const osc = audioCtx.createOscillator();
-        osc.type = "sine";
-        osc.frequency.setValueAtTime(pitchValue, audioCtx.currentTime)
-        osc.connect(audioCtx.destination);
-        if (pulse.includes(".")) {
+  const translateEachPulse = async (pulse, pitchValue, stopValue) => {
+    let oscillator = inizialiceOsc(pitchValue, stopValue);
+        if (stopValue) {
+          oscillator.stop();
+          oscillator.disconnect(gainNode).disconnect(audioCtx.destination);
+        } else if (pulse.includes(".")) {
           setBinaryPulse(".");
-          osc.start();
+          oscillator.start();
           await speed(speedValue); // Detener la ejecución durante x segundos
           setBinaryPulse("");
-          osc.stop();
-          osc.connect(audioCtx.destination);
+          oscillator.stop();
           await speed(speedValue/2); 
         } else if (pulse.includes("-")) {
           setBinaryPulse("-");
-          osc.start();
-          await speed(speedValue * 2); // Detener la ejecución durante x segundos
+          oscillator.start();
+          await speed(speedValue * 2);
           setBinaryPulse("");
-          osc.stop();
-          osc.connect(audioCtx.destination);
+          oscillator.stop();
           await speed(speedValue/2);
         } else if (pulse.includes("|")) {
           setBinaryPulse("");
-          osc.connect(audioCtx.destination);
           await speed(speedValue);
-        }
+        } 
+  }
+
+  const lightTranslation = async() => {
+    if (morsePulseArray.length !== 0 && !stopValue) {
+      for (const pulse of morsePulseArray) {
+        await translateEachPulse(pulse, pitchValue, stopValue)
+      }
       }
       setBinaryPulse("")
-    }
   }
 
-  const lightMode = () => {
+  const lightMode = (stop, pitch) => {
     readMorse();
-    lightTranslation();
+    lightTranslation(stop, pitch);
   }
-
- 
 
 
   /* Renderizado */
   return (
-    <div>
-      <textarea id="stringTextarea" name="stringTextarea" placeholder="Introduce el texto a traducir" cols={30} rows={30} onChange={(e) => {
-        sendData(e.target.value);
-      }} />
-      <textarea id="morseTextarea" name="morseTextarea" cols={30} rows={30} value={morseData === "" ? "Introduce el texto a traducir" : morseData}/>
-      <button onClick={() => lightMode()}>Light translate</button>
-      
-      <div>
-        <input
-          type="range" 
-          id="speed" 
-          name="speed" 
-          min={20} 
-          max={1000}
-          value={speedValue}
-          onChange={ e => setSpeedValue(e.target.value)} />
-        <label htmlFor="speed">Speed</label>
-        <label htmlFor="speed">{` (${speedValue} ${speedValue < 1000 ? "ms" : "seg"})`}</label>
+    <div className={`${"main"}`}>
+      <div className={`${"textareaMain"} ${binaryPulse !== "" ? "light" : "dark"}`}>
+        <textarea className={`${"textareaStyle"} `} id="stringTextarea" name="stringTextarea" placeholder="Introduce el texto a traducir" cols={60} rows={10}  onChange={(e) => {
+          sendData(e.target.value);
+        }} />
+        <textarea className={`${"textareaStyle"} `} id="morseTextarea" name="morseTextarea" cols={60} rows={10} value={morseData === "" ? "Introduce el texto a traducir" : morseData}/>
       </div>
       <div>
-        <input
-          type="range" 
-          id="pitch" 
-          name="pitch" 
-          min={100} 
-          max={1000}
-          value={pitchValue}
-          onChange={ e => setPitchValue(e.target.value)} />
-        <label htmlFor="pitch">Pitch</label>
-        <label htmlFor="pitch">{` (${pitchValue} Hz)`}</label>
+        <div className={"buttonsMain"}>
+          <button 
+          className={"shadow-inset-center buttonStyle"} 
+          onClick={() => {
+            lightMode(stopValue, pitchValue);
+            if (morsePulseArray.length !== 0) {
+              setPlayValue(!playValue); 
+            }
+            }}>
+            {
+              playValue ? <FontAwesomeIcon icon={faPause}/> : <FontAwesomeIcon icon={faPlay}/>
+            }
+          </button>
+          <button 
+          className={"shadow-inset-center buttonStyle"} 
+          onClick={() => {setStopValue(!stopValue)}}>
+            {
+              stopValue ? <FontAwesomeIcon icon={faStop}/> : <FontAwesomeIcon icon={faStop}/>
+            }
+          </button>
+          <button 
+          className={"shadow-inset-center buttonStyle"} 
+          onClick={() => setMuteValue(!muteValue)}>
+          {
+          muteValue ? <FontAwesomeIcon icon={faVolumeXmark} /> : 
+          (volumeValue >= 1.5 ? <FontAwesomeIcon icon={faVolumeHigh} /> : 
+          volumeValue >= 0.5 ? <FontAwesomeIcon icon={faVolumeLow} /> : <FontAwesomeIcon icon={faVolumeOff}/>) 
+          }
+          </button>
+        </div>
+        <div className={"inputRangesMain"}>
+          <div className={"inputRange"}>
+            <input
+              type="range"
+              id="speed"
+              name="speed"
+              min={20}
+              max={1500}
+              value={speedValue}
+              onChange={ e => setSpeedValue(e.target.value)} />
+            <label htmlFor="speed">Speed</label>
+            <label htmlFor="speed">{` (${speedValue} ${speedValue < 1000 ? "ms" : "seg"})`}</label>
+          </div>
+          <div className={"inputRange"}>
+            <input
+              type="range"
+              id="pitch"
+              name="pitch"
+              min={100}
+              max={1000}
+              value={pitchValue}
+              onChange={ e => setPitchValue(e.target.value)} />
+            <label htmlFor="pitch">Pitch</label>
+            <label htmlFor="pitch">{` (${pitchValue} Hz)`}</label>
+          </div>
+          <div className={"inputRange"}>
+            <input
+              type="range"
+              id="volume"
+              name="volume"
+              min={0}
+              max={2}
+              step={0.01}
+              value={muteValue === true ? 0 : volumeValue}
+              onChange={ e => setVolumeValue(e.target.value)} />
+            <label htmlFor="volume">Volume</label>
+          </div>
+        </div>
       </div>
-      <div style={{ height: "40vh", width: "5vw", border: "1px solid red", backgroundColor: binaryPulse !== "" ? "white" : "black"}}></div>
     </div>
   );
 }
